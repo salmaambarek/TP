3c9fa7c198928852ac80a951739976f5b134f3c4
salma <mbarek.salmaa@gmail.com>
2019-03-29 21:51:14 +0100

    ajout php

---
+++ php/inscription.php
@@ -0,0 +1,18 @@
+<?php 
+$nom=$_GET['nom'];
+$prenom=$_GET['prenom'];
+$tel=$_GET['Tel'];
+$gender=$_GET['gender'];
+$date_naissance=$_GET['date_naissance'];
+$nationalite=$_GET['nationalite'];
+$staticEmail2=$_GET['staticEmail2'];
+$inputPssword2=$_GET['inputPssword2'];
+echo $nom.'<br/>'; 
+echo $prenom.'<br/>';
+echo $Tel.'<br/>' ;
+echo $gender'<br/>' ;
+echo $date_naissance.'<br/>'; ; 
+echo $nationalite.'<br/>'; ;
+ echo $staticEmail2.'<br/>';;
+echo $inputPssword2.'<br/>';;
+?>
